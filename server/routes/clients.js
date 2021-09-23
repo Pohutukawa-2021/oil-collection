6f398265489309dddbca4fb669cbda63fdc0460d
Dave-Haworth <david.haworth@xtra.co.nz>
2021-09-24 10:31:58 +1200

    progress

---
+++ server/routes/clients.js
@@ -0,0 +1,31 @@
+const express = require('express')
+
+const db = require('../db/clients')
+const { getUserRoles } = require('../auth0')
+
+const router = express.Router()
+
+module.exports = router
+
+router.post('/', (req, res) => {
+  const newUser = req.body
+  db.addUser(newUser)
+    .then(() => {
+      res.sendStatus(201)
+      return null
+    })
+    .catch(err => {
+      res.status(500).json({ error: err.message })
+    })
+})
+
+router.get('/:id', async (req, res) => {
+  const id = req.params.id
+  try {
+    const roles = await getUserRoles(id)
+    res.json({ roles })
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).json({ message: 'unable to retrieve user roles' })
+  }
+})
