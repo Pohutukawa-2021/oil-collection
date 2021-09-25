7bce1480ebe15a35a9ae4902e41ef7c89bbd07c5
Zahira Champion <zahira.ali.champion@gmail.com>
2021-09-25 12:16:19 +1200

    create updateCustomerDetails route with test passing

--- server/routes/clients.test.js
+++ server/routes/clients.test.js
@@ -31,3 +31,29 @@ test('UPDATE order active status', () => {
       return null
     })
 })
+
+test('UPDATE customer details', () => {
+  db.updateCustomerDetails = jest.fn()
+  db.updateCustomerDetails.mockImplementation(() => {
+    return Promise.resolve({
+      id: 2,
+      first_name: 'Zahira',
+      last_name: 'Champion',
+      business_name: 'CookSavvy',
+      address_street: '1A Lilac Hwy',
+      address_suburb: 'CBD',
+      address_city: 'Auckland',
+      product: 'fat',
+      containers: 'drum'
+    })
+  })
+  return request(server)
+    .patch('/api/v1/clients/2/update')
+    .then((updateDetails) => {
+      expect(updateDetails.status).toBe(200)
+      expect(updateDetails.body.first_name).toBe('Zahira')
+      expect(updateDetails.body.address_street).toBe('1A Lilac Hwy')
+      expect(updateDetails.body.product).toBe('fat')
+      return null
+    })
+})
