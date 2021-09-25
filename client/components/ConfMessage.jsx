f76b91841b7d702decf8ad0d60e56f4f7d0ae7cb
Zahira Champion <zahira.ali.champion@gmail.com>
2021-09-26 11:19:01 +1300

    create ConfMessage component

--- client/components/ConfMessage.jsx
+++ client/components/ConfMessage.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
-const ConfMessage = () => (
-  <div>
-  </div>
-)
+function ConfMessage (props) {
+  return (
+    <>
+      <div>
+        <p>
+          Collection Confirmed! <br></br>
+          Your collection will be picked up within 48 hours. <br></br>
+          If you have any questions please call 0800 009 008. <br></br>
+        </p>
+      </div>
+    </>
+  )
+}
 
 export default ConfMessage
