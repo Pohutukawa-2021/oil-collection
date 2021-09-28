1cef1fa8f5f88ca77c9020e1811eb5f743724fcb
Zahira Champion <zahira.ali.champion@gmail.com>
2021-09-28 14:00:51 +1300

    merging dev into mine

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -54,50 +54,6 @@ function Nav (props) {
   )
 }
 
-function Nav (props) {
-  // const { user } = props
-  const login = getLoginFn(useAuth0)
-  const logout = getLogoutFn(useAuth0)
-  const register = getRegisterFn(useAuth0)
-
-  function handleLogin (event) {
-    event.preventDefault()
-    login()
-  }
-
-  function handleLogoff (event) {
-    event.preventDefault()
-    logout()
-  }
-
-  function handleRegister (event) {
-    event.preventDefault()
-    register()
-  }
-
-  return (
-    <nav className='nav'>
-      <section className='nav-item'>
-        <IfAuthenticated>
-          {/* <p>Hello, {user.name}</p> */}
-          <section className='sign'>
-            <a href='/' onClick={handleLogoff} className='nav__link'>Log out</a>
-          </section>
-        </IfAuthenticated>
-        <IfNotAuthenticated>
-          {/* <section className='nav-item'> */}
-          <p>Hello, guest</p>
-          <section className='sign'>
-            <a href='/' onClick={handleLogin} className='nav__link'>Sign in</a>
-            <a href='/' onClick={handleRegister} className='nav__link'>Register</a>
-          </section>
-          {/* </section> */}
-        </IfNotAuthenticated>
-      </section>
-    </nav >
-  )
-}
-
 function mapStateToProps (state) {
   return {
     user: state.user
