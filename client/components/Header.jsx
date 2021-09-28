e03ef613728156a6f551770a09e12e478a3942f6
Zahira Champion <zahira.ali.champion@gmail.com>
2021-09-28 13:28:39 +1300

    style nav

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
-function Header() {
+// const backgroundImage = {
+//   backgroundImage: 'url("../Oil-Collection.png")'
+// }
+
+function Header () {
   return (
     <header>
-      {/* <Link className='logo' to="/"><img src="./logo.jpg" alt="logo" /></Link> */}
-      <h1><Link className='logo' to="/">Oil Collection</Link></h1>
+      {/* <Link className='logo-link' to="/"><div className='logo' style={backgroundImage}></div></Link> */}
+      {/* <h1><Link className='logo' to="/">Oil Collection</Link></h1> */}
     </header>
   )
 }
